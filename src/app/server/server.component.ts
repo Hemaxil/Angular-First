3c81f45a5fc1c5a454d1a89307558b63a32606c0
Webwerks-Pc <hemaxilade@gmail.com>
2019-03-20 12:33:45 +0530

    event,property binding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -5,7 +5,14 @@ import { Component } from '@angular/core';
   	templateUrl: './server.component.html',
 })
 
-export class ServerComponent{}
+export class ServerComponent{
+serverId:number =10;
+serverStatus:string ="offline";
+
+getServerStatus(){
+	return this.serverStatus;
+}
+}
 
 
 
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,9 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+	allowNewServer=false;
+	serverCreationStatus="server not created";
+  serverName='';
+  constructor() {
 
-  constructor() { }
+  setTimeout(()=>{
+  	this.allowNewServer=true;
+  },2000);
+   }
 
+   onServerCreation(){
+   	this.serverCreationStatus="Server created";
+   }
+
+   onUpdateServerName(event){
+    // console.log(event.target.value);
+    this.serverName=(<HTMLInputElement>event.target).value;
+    
+   }
   ngOnInit() {
   }
 
